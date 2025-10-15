4d2293d2aa762affc0da6fe7824a2cac506fb5a9
kelvin-trabalho <kelvinkelvinbarroso@gmail.com>
2025-10-15 11:32:38 -0300

    Condições

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 99
+console.log(`Você ${idade} anos.`)
+if (idade < 16){
+    console.log('Não vota')
+} else if (idade < 18  || idade > 65) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
